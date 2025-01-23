5e2700a407728258f4b43c84237b136e0633dde5
Happy <145888543+3sut2z@users.noreply.github.com>
2025-01-23 18:06:02 +0700

    Update bypass.py

--- bypass.py
+++ bypass.py
@@ -15,4 +15,4 @@ def add_link():
         return jsonify({"status": "error", "message": "Missing URL parameter"}), 400
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5050, debug=True)
