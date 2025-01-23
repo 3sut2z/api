459be4f9b5673ed68bc1e4eb06977f82fc1a45b9
Happy <145888543+3sut2z@users.noreply.github.com>
2025-01-23 18:19:16 +0700

    Update bypass.py

--- bypass.py
+++ bypass.py
@@ -3,15 +3,15 @@ import time
 
 app = Flask(__name__)
 
-@app.route('/addlink', methods=['GET'])
+@app.route('/api-bypass/addlink', methods=['GET'])
 def add_link():
     url = request.args.get('url')
     if url:
         start_time = time.time()
         duration = time.time() - start_time
-        return f'{{"status":"success","result":"Api Bypass Is In Progress","duration":"{duration:.6f}"}}'
+        return f'<pre style="font-family: monospace;">{{"status":"success","result":"Api Bypass Is In Progress","duration":"{duration:.6f}"}}</pre>'
     else:
-        return f'{{"status":"error","result":"Missing URL parameter"}}', 400
+        return f'<pre style="font-family: monospace;">{{"status":"error","message":"Missing URL parameter"}}</pre>', 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050, debug=True)
