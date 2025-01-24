c3c63c9e9b59f167bdcf85d0da898f97385e5d75
Happy <145888543+3sut2z@users.noreply.github.com>
2025-01-24 10:04:24 +0700

    Update bypass.py

--- bypass.py
+++ bypass.py
@@ -12,12 +12,11 @@ app = Flask(__name__)
 def bypass_linkvertise(url):
     try:
         chrome_options = Options()
-chrome_options.binary_location = "C:\Users\Administrator\Downloads\ChromeSetup.exe"
-chrome_options.add_argument("--headless")
-chrome_options.add_argument("--no-sandbox")
-chrome_options.add_argument("--disable-dev-shm-usage")
+        chrome_options.add_argument("--headless")
+        chrome_options.add_argument("--no-sandbox")
+        chrome_options.add_argument("--disable-dev-shm-usage")
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
+        driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=chrome_options)
 
         driver.get(url)
 
