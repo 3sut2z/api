7373deee3b3756767a9400e9099d4bad443d1c9f
Happy <145888543+3sut2z@users.noreply.github.com>
2025-01-24 09:09:14 +0700

    Update bypass.py

--- bypass.py
+++ bypass.py
@@ -9,9 +9,9 @@ def add_link():
     if url:
         start_time = time.time()
         duration = time.time() - start_time
-        return f'<pre style="font-family: monospace;">{{"status":"success","result":"Api Bypass Is In Progress","duration":"{duration:.6f}"}}</pre>'
+        return f'<pre style="font-family: monospace;">{{"status":"success","result":"Api Bypass Is In Progress","duration":"{duration:.16f}"}}</pre>'
     else:
-        return f'<pre style="font-family: monospace;">{{"status":"error","message":"Missing URL parameter"}}</pre>', 400
+        return f'<pre style="font-family: monospace;">{{"status":"error","result":"Missing URL parameter"}}</pre>', 400
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5050, debug=True)
